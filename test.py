5659a8fbf1226d390559f3d094d3acca2046a759
charlie <Abc12345>
2018-03-19 16:28:18 +0800

    string list

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+a = 'first'
+b = 'seconde'
+
+if a == "first" :
+    print('true', a)
+else :
+    print(a, b)
+
+num = ['a', 2, 3.3, 'four']
+mun = ['third', 'two']
+#print(num + mun)
+print(num * 2)
+print(num)
+
+#mun = mun * 2
+#print(mun)
